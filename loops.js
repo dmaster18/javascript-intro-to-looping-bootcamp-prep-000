82097ef6a0cf03172562b637d81f3d0fa1b0f70b
David Strauch <dmaster18@gmail.com>
2019-06-12 15:45:26 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,36 @@
+const array = [];
+function forLoop(array){
+  for (let i = 0; i < 25; i++)
+  {
+    if (i === 1) {
+    array.push( `I am ${i} strange loop.`);
+    }
+    else {
+    array.push( `I am ${i} strange loops.`);
+    }
+  }
+  return array;
+}
+let number = 10;
+function whileLoop(number){
+  while (number > 0)
+  {console.log(number);
+  number--;
+}
+return "done";
+}
+
+let i = 23;
+function incrementVariable() {
+  i = i + 1;
+  return i;
+}
+function doWhileLoop(i) {
+  do {
+    console.log("I run once regardless.");
+  }while(incrementVariable(i));
+}
+
+
+
+
